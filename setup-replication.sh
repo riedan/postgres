1f1641f56ff11c415429c99e1694d9e994c5a1dd
jsron <jsron@DESKTOP-RVGHU4J>
2019-05-29 11:28:58 +0200

    only replication user can connect for the replication

--- setup-replication.sh
+++ setup-replication.sh
@@ -2,7 +2,7 @@
 
 if [ "x$REPLICATE_FROM" == "x" ]; then
 
-echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+echo "host replication $PG_REP_USER 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
@@ -31,7 +31,7 @@ do
 echo "Waiting for master to connect..."
 sleep 1s
 done
-echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+echo "host replication $PG_REP_USER 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 set -e
 cat > ${PGDATA}/recovery.conf <<EOF
 standby_mode = on
