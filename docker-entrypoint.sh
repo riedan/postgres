34684d1a91a3e9d90add5a483155423707255b97
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2019-07-15 17:01:28 +0200

    fix condition

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -Eeo pipefail
 # TODO swap to -Eeuo pipefail above (after handling all potentially-unset variables)
 
-if [  -z "$(ls -A /certs/)" ]; then
+if [ "$(ls -A /certs/)" ]; then
   cp /certs/* /usr/local/share/ca-certificates/.
   update-ca-certificates
 fi
