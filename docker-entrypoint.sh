4bdc278d27b39cd49fb5b603d5c84cdc325884eb
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2019-07-16 12:01:17 +0200

    update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -Eeo pipefail
 # TODO swap to -Eeuo pipefail above (after handling all potentially-unset variables)
 
 if [ "$(ls -A /certs/)" ]; then
-  cp -n /certs/* /usr/local/share/ca-certificates/
+  cp -T -u /certs/* /usr/local/share/ca-certificates/
   chmod 644 /usr/local/share/ca-certificates/*
   update-ca-certificates
 fi
