e055cdd4c9c8049c1f9b12acebdb8ab694dcf8ef
riedan <50929933+riedan@users.noreply.github.com>
2019-10-22 11:09:57 +0200

    hide password

--- script/03-register_node.sh
+++ script/03-register_node.sh
@@ -10,15 +10,15 @@ installed=$(psql -qAt -h "$PGHOST" -U "$PG_REP_USER" --dbname "$PG_REP_DB" -p "$
 if [ "${installed}" != "1" ]; then
     echo '~~ 03: registering as primary' >&2
     repmgr -f ${PG_CONFIG_DIR}/repmgr.conf primary register -k 30
-    export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+    export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}" > /dev/null
     return
 fi
 
 if [ -n "$WITNESS" ]; then
 	echo '~~ 03: registering as witness server' >&2
   repmgr -f ${PG_CONFIG_DIR}/repmgr.conf -h "$PRIMARY_NODE" -U "$PG_REP_USER" -d "$REPMGR_DB" -p "$PG_PORT" witness register -k 30
-  export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
-	return
+  export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}" > /dev/null
+  return
 fi
 
 my_node=$(grep node_id ${PG_CONFIG_DIR}/repmgr.conf | cut -d= -f 2)
