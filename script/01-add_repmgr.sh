987c132572bfad40fbb08a2ba03b179a38632716
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2019-10-22 12:57:36 +0200

    rollback

--- script/01-add_repmgr.sh
+++ script/01-add_repmgr.sh
@@ -4,6 +4,39 @@ set -e
 
 
 
+cp /usr/local/share/postgresql/postgresql.conf.repmgr $PGDATA/postgresql.conf
+
+if  [ -n "${PG_SSL}" ]; then
+
+  sed -i "s@#*.*\(ssl =\).*@\1 ${PG_SSL}@;" ${PGDATA}/postgresql.conf
+
+
+  if  [ -n "${PG_SSL_KEY_FILE}" ]; then
+    cp ${PG_SSL_KEY_FILE} ${PG_CONFIG_DIR}/server.key
+    chown  ${SYS_USER}:${SYS_GROUP}  ${PG_CONFIG_DIR}/server.key
+    chmod 600  ${PG_CONFIG_DIR}/server.key
+  fi
+
+  if  [ -n "${PG_SSL_CERT_FILE}" ]; then
+    cp ${PG_SSL_CERT_FILE} ${PG_CONFIG_DIR}/server.crt
+    chown  ${SYS_USER}:${SYS_GROUP}  ${PG_CONFIG_DIR}/server.crt
+    chmod 600  ${PG_CONFIG_DIR}/server.key
+  fi
+
+  if  [ -n "${PG_SSL_CA_FILE}" ]; then
+    cp ${PG_SSL_CA_FILE} ${PG_CONFIG_DIR}/root.crt
+    chown  ${SYS_USER}:${SYS_GROUP}  ${PG_CONFIG_DIR}/root.crt
+    chmod 600  ${PG_CONFIG_DIR}/server.key
+  fi
+
+  sed -i "s@#*.*\(ssl_cert_file =\).*@\1 \'${PG_CONFIG_DIR}/server.crt\'@;" ${PGDATA}/postgresql.conf
+  sed -i "s@#*.*\(ssl_key_file =\).*@\1 \'${PG_CONFIG_DIR}/server.key\'@;" ${PGDATA}/postgresql.conf
+  sed -i "s@#*.*\(ssl_ca_file =\).*@\1 \'${PG_CONFIG_DIR}/root.crt\'@;" ${PGDATA}/postgresql.conf
+
+fi
+
+
+
 if [ $(grep -c "replication repmgr" ${PGDATA}/pg_hba.conf) -gt 0 ]; then
     return
 fi
