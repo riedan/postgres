1ff3810cca69978a9dfd85eba4fc30c6007607e7
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2020-01-17 17:43:27 +0100

    remove

--- script/entrypoint.py
+++ script/entrypoint.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 import sys
-from entrypoint_helpers import env, gen_cfg, gen_container_id, str2bool, start_app,  set_perms, set_ownership, activate_ssl
+from entrypoint_helpers import env, gen_cfg, gen_container_id, str2bool, start_app,  set_perms, set_ownership
 
 
 RUN_USER = env['sys_user']
