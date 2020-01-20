f98f06a376fe6f1c04c151494fe57224d2296b95
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2020-01-20 14:50:38 +0100

    fix post_boostrap script

--- script/post_bootstrap.sh
+++ script/post_bootstrap.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "\c template1
-CREATE DATABASE $POSTGRES_DB;" \
-|  psql -d $1 # $1 - connection string to the newly created master.
+echo "\x \\ CREATE DATABASE $POSTGRES_DB;" \
+|  psql "$1" # $1 - connection string to the newly created master.
