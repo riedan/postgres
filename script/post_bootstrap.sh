8e3e7fd00a76a24220901082743d250c6cad3b6e
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2020-01-20 15:24:09 +0100

    fix

--- script/post_bootstrap.sh
+++ script/post_bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "\x \\ CREATE DATABASE $POSTGRES_DB;" |  psql "$1" # $1 - connection string to the newly created master.
+echo "CREATE DATABASE $POSTGRES_DB;" |  psql "$1" # $1 - connection string to the newly created master.
